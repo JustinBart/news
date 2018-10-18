b243ac5759baa7d002ddcb57801fe06ade73751f
Justin Bartholomew <justin.bartholomew@opsysinc.com>
2018-10-18 07:59:39 -0600

    setting up loader and main activity

--- app/src/main/java/com/example/android/news/MainActivity.java
+++ app/src/main/java/com/example/android/news/MainActivity.java
@@ -1,17 +1,37 @@
 package com.example.android.news;
 
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //todo progress bar
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements LoaderCallbacks<List<News>> {
 
     /** TextView that is displayed when the list is empty */
     private TextView mEmptyStateTextView;
 
+    /** Adapter for the list of news articles */
+    private NewsAdapter mAdapter;
+
     private static final String REQUEST_URL =
             "http://content.guardianapis.com/search?q=debates&api-key=test&show-tags=contributor";
+    /**
+     * Constant value for the earthquake loader ID. We can choose any integer.
+     * This really only comes into play if you're using multiple loaders.
+     */
+    private static final int NEWS_LOADER_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +40,55 @@ public class MainActivity extends AppCompatActivity {
 
         ListView newsListView = findViewById(R.id.list);
 
-        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        mEmptyStateTextView = findViewById(R.id.empty_view);
+        newsListView.setEmptyView(mEmptyStateTextView);
+
+        mAdapter = new NewsAdapter(this, new ArrayList<News>());
+
+        newsListView.setAdapter(mAdapter);
+
+        //todo setOnItemClickListener to link user to url
+
+
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
+    }
+
+    @Override
+    public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> news) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+
     }
 }
---
+++ app/src/main/java/com/example/android/news/NewsAdapter.java
@@ -0,0 +1,63 @@
+package com.example.android.news;
+
+import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class NewsAdapter extends ArrayAdapter {
+
+    /**
+     * LocationAdapter constructor
+     * @param context the context
+     * @param locations the location to build
+     */
+    public NewsAdapter(Context context, ArrayList<News> locations) {
+        super(context, 0, locations);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        // check if the existing view is being reused, otherwise inflate the view
+        if(convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // get's the location at the given position
+        News currentNews = (News) getItem(position);
+
+        // get the title
+        TextView title = convertView.findViewById(R.id.title);
+        title.setText(currentNews.getTitle());
+
+        // get the type/section
+        TextView type = convertView.findViewById(R.id.type);
+        type.setText(currentNews.getSection() + ": " + currentNews.getType());
+
+        // get the date
+        TextView date = convertView.findViewById(R.id.datePublished);
+        date.setText(currentNews.getDatePublished());
+
+        TextView authorView = convertView.findViewById(R.id.author);
+        String author = currentNews.getAuthor();
+
+        if (author != null) {
+            authorView.setText(author);
+            authorView.setVisibility(View.VISIBLE);
+        } else {
+            authorView.setVisibility(View.GONE);
+        }
+
+        return convertView;
+    }
+}
